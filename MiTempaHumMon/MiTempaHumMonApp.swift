8347e6f1aec048e8a4a8861590d25ced23df1023
Bayramgeldi Bayryyev <bayramgeldi.bayriyev@botguard.ee>
2024-12-26 17:30:00 +0300

    Initial Commit

---
+++ MiTempaHumMon/MiTempaHumMonApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiTempaHumMonApp.swift
+//  MiTempaHumMon
+//
+//  Created by bayramgeldi on 26.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MiTempaHumMonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MiTempaHumMonTests/MiTempaHumMonTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiTempaHumMonTests.swift
+//  MiTempaHumMonTests
+//
+//  Created by bayramgeldi on 26.12.2024.
+//
+
+import Testing
+@testable import MiTempaHumMon
+
+struct MiTempaHumMonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
